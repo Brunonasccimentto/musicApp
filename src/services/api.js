e1c3eec016ee223608815aa7d01f928e31da3bcf
Brunonasccimentto <104613598+Brunonasccimentto@users.noreply.github.com>
2022-09-29 01:23:22 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://nodejs-express-login.herokuapp.com/",
+    baseURL: "https://localhost:3001",
 })
 
 export const createUser = async (name, email, password)=>{
